b710700cb8ce4115a70739d2b83231a15ae501ce
Asaf Sandro Smirnov <asafsmirnov@gmail.com>
2025-12-10 20:28:03 +0200

    Add scoring

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -5,12 +5,16 @@ const MARGIN: float = 80.0
 const STOPPABLE_GROUP: String = "stoppable"
 
 @onready var spawn_timer: Timer = $SpawnTimer
+@onready var score_label: Label = $ScoreLabel
+
+var _points: int = 0
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("restart"):
 		get_tree().reload_current_scene()
 
 func _ready() -> void:
+	update_score_label()
 	spawn_dice()
 
 func _on_spawn_timer_timeout() -> void:
@@ -33,8 +37,12 @@ func pause_all() -> void:
 	for item in to_stop:
 		item.set_physics_process(false)
 
+func update_score_label() -> void:
+	score_label.text = "%04d" % _points
+
 func _on_dice_game_over() -> void:
 	pause_all()
 
 func _on_fox_point_scored() -> void:
-	print("point scored")
+	_points += 1
+	update_score_label()
